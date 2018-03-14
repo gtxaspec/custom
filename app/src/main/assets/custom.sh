72c76b9b668a425c7c4173c311a11b27b4a02ee7
gtxent <gtxent@gmail.com>
2018-03-13 23:59:03 -0700

    add performance mode to custom.sh

--- app/src/main/assets/custom.sh
+++ app/src/main/assets/custom.sh
@@ -5,17 +5,12 @@ p10_install="a2a36f22d153929f0b5e39a3f358c5e8922dd15c"
 stock_sha="d11c3b52dd8f3191e949e755fb5c53f203ab179a"
 custom=0
 
-###
-###install.sh
-###
-
 install_custom_recovery() {
 
 install_check() {
 
 if [ `getprop | grep ro.build.date.utc | sed 's/.*\[\([^]]*\)\].*/\1/g'` -ge "1513323821" ]; then
 
-#check integrity of installation files
 echo "Recovery Files Checksum Verification..."
 if [ `sha1sum /data/data/com.gtx.jy.custom/p10_stock_rec.img | cut -c1-40` == $stock_sha ] && [ `sha1sum /data/data/com.gtx.jy.custom/recovery_new_keys.img | cut -c1-40` == $p10_sha ]; then
     echo "Checksums Match for Recovery Files..."
@@ -30,7 +25,7 @@ fi
 }
 
 install_custom(){
-#install custom recovery
+
 if [ $custom == "0" ]; then
     echo "Installing CUSTOM recovery..."
     dd if=/data/data/com.gtx.jy.custom/recovery_new_keys.img of=/dev/block/mmcblk0p10
@@ -43,7 +38,7 @@ fi
 }
 
 install_verify(){
-#verify installed recovery integrity
+
 echo "Verifying Installation Integrity..."
 dd if=/dev/block/mmcblk0p10 of=/cache/p10.img
 if [ `sha1sum /cache/p10.img | cut -c1-40` == $p10_install ]; then
@@ -60,7 +55,6 @@ fi
 
 
 install_failed(){
-#failed both installs.
 
 echo "Signature Verification FAILED! Twice! Falling back to STOCK Recovery..."
 dd if=/data/data/com.gtx.jy.custom/p10_stock_rec.img of=/dev/block/mmcblk0p10
@@ -77,11 +71,6 @@ install_check
 
 
 }
-
-#####end install.sh
-
-
-###restore.sh
 
 restore_stock_recovery() {
 
@@ -120,10 +109,6 @@ restore
 
 }
 
-#####end restore.sh
-
-####VEIRFY
-
 verify_custom_recovery() {
 dd if=/dev/block/mmcblk0p10 of=/cache/p10_verify.img
 
@@ -139,13 +124,6 @@ fi
 
 rm -f /cache/p10*
 }
-
-###END
-####END VERIFY
-
-
-
-###begin reboot custom
 
 reboot_custom_lbdroid() {
 mount=`find /mnt -maxdepth 5 -name "5009_60_gtx.zip" -exec echo "{}" \; | grep -E '/mnt/usb_storage|/mnt/external_sd|/mnt/external_sdio' | sed -r 's_^(/[^/]*){1}/([^/]*)/.*$_\2_g'`
@@ -169,18 +147,11 @@ fi
 touch /cache/recovery/command
 echo "--update_package=/cache/5009_60.zip" > /cache/recovery/command
 rm -f /cache/p10*
-##reboot now
 
-i2cset -f -y 4 0x40 0x06 0xff
-i2cset -f -y 4 0x40 0x20 0x00
-echo 1 > /sys/fytver/muteAMP
+silence_amp
 
 reboot recovery
 }
-
-###end reboot custom
-
-###begin reboot_gtx
 
 reboot_custom_gtx() {
 mount=`find /mnt -maxdepth 5 -name "5009_60_gtx.zip" -exec echo "{}" \; | grep -E '/mnt/usb_storage|/mnt/external_sd|/mnt/external_sdio' | sed -r 's_^(/[^/]*){1}/([^/]*)/.*$_\2_g'`
@@ -204,17 +175,11 @@ fi
 touch /cache/recovery/command
 echo "--update_package=/cache/5009_60.zip" > /cache/recovery/command
 rm -f /cache/p10*
-##reboot now
 
-i2cset -f -y 4 0x40 0x06 0xff
-i2cset -f -y 4 0x40 0x20 0x00
-echo 1 > /sys/fytver/muteAMP
+silence_amp
 
 reboot recovery
 }
-###END REBOOT GTX
-
-##REBOOT TO TWRP
 
 reboot_to_twrp() {
 echo "Rebooting..."
@@ -225,24 +190,10 @@ touch /cache/recovery/command
 echo "--update_package=/cache/5009_60.zip" > /cache/recovery/command
 rm -f /cache/p10*
 
-##reboot now
-i2cset -f -y 4 0x40 0x06 0xff
-i2cset -f -y 4 0x40 0x20 0x00
-echo 1 > /sys/fytver/muteAMP
-
-printf '\x62\x6f\x6f\x74\x2d\x72\x65\x63\x6f\x76\x65\x72\x79\x00\x00\x00' | busybox dd of=/dev/block/mmcblk0p17 bs=1 seek=16384 count=16 conv=notrunc 
-
-printf '\x72\x65\x63\x6f\x76\x65\x72\x79\x0a\x2d\x2d\x75\x70\x64\x61\x74' | busybox dd of=/dev/block/mmcblk0p17 bs=1 seek=16448 count=16 conv=notrunc 
-printf '\x65\x5f\x70\x61\x63\x6b\x61\x67\x65\x3d\x2f\x63\x61\x63\x68\x65' | busybox dd of=/dev/block/mmcblk0p17 bs=1 seek=16464 count=16 conv=notrunc 
-printf '\x2f\x35\x30\x30\x39\x5f\x36\x30\x2e\x7a\x69\x70\x0a\x00\x00\x00' | busybox dd of=/dev/block/mmcblk0p17 bs=1 seek=16480 count=16 conv=notrunc 
-
-
+silence_amp
 
 reboot recovery
 }
-##END REBOOT TO TWRP
-
-###BEGIN TOUCH
 
 touch_upgrade() {
 
@@ -252,7 +203,34 @@ echo "no_wipe_data" >> /data/.gtx_upgrade
 
 }
 
+silence_amp() {
 
+i2cset -f -y 4 0x40 0x06 0xff
+i2cset -f -y 4 0x40 0x20 0x00
+echo 1 > /sys/fytver/muteAMP
+
+}
+
+recovery_bcb() {
+
+printf '\x62\x6f\x6f\x74\x2d\x72\x65\x63\x6f\x76\x65\x72\x79\x00\x00\x00' | busybox dd of=/dev/block/mmcblk0p17 bs=1 seek=16384 count=16 conv=notrunc 
+printf '\x72\x65\x63\x6f\x76\x65\x72\x79\x0a\x2d\x2d\x75\x70\x64\x61\x74' | busybox dd of=/dev/block/mmcblk0p17 bs=1 seek=16448 count=16 conv=notrunc 
+printf '\x65\x5f\x70\x61\x63\x6b\x61\x67\x65\x3d\x2f\x63\x61\x63\x68\x65' | busybox dd of=/dev/block/mmcblk0p17 bs=1 seek=16464 count=16 conv=notrunc 
+printf '\x2f\x35\x30\x30\x39\x5f\x36\x30\x2e\x7a\x69\x70\x0a\x00\x00\x00' | busybox dd of=/dev/block/mmcblk0p17 bs=1 seek=16480 count=16 conv=notrunc 
+
+}
+
+enable_performance_mode() {
+
+mount -o rw,remount /system
+
+mv /system/priv-app/ituxd/ituxd.apk /system/priv-app/ituxd/ituxd.apk.old
+
+sed -i 's#echo interactive | tee /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor#echo performance | tee /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor#g' /system/bin/install-recovery.sh
+sed -i '113s#echo 416000 | tee /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq;#echo 1200000 | tee /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq;#g' /system/bin/install-recovery.sh
+sed -i '117s#echo 416000 | tee /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq;#echo 1040000 | tee /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq;#g' /system/bin/install-recovery.sh
+
+}
 
 if [ "$1" == "install_custom_recovery" ]; then
 install_custom_recovery
